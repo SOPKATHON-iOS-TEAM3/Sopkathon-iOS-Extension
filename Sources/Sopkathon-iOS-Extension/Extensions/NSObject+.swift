05568c57f25b28c8cf7b1fadb29d466eee6b5c7f
ikbum <parkikbum>
2024-05-18 01:14:01 +0900

    [Fix]: fix reuseidentifier

--- Sources/Sopkathon-iOS-Extension/Extensions/NSObject+.swift
+++ Sources/Sopkathon-iOS-Extension/Extensions/NSObject+.swift
@@ -1,11 +1,7 @@
 import UIKit
 
-public protocol ReuseIdentifier {
-    static var reuseIdentifier: String { get }
-}
-
-extension NSObject: ReuseIdentifier {
-    public static var reuseIdentifier: String {
+public extension NSObject {
+    static var reuseIdentifier: String {
         return String(describing: self)
     }
 }
--- Sources/Sopkathon-iOS-Extension/Extensions/UIStackView+.swift
+++ Sources/Sopkathon-iOS-Extension/Extensions/UIStackView+.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 public extension UIStackView {
-    func addArrangeSubViews(_ views: UIView...) {
+    func addArrangeSubviews(_ views: UIView...) {
         views.forEach { self.addArrangedSubview($0) }
     }
 }
