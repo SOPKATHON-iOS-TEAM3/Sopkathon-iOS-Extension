82ae1b1fa85036b513f8246f068d549f20f19b80
박익범 <parkikbum@bag-igbeom-ui-MacBookPro.local>
2024-05-17 23:57:03 +0900

    [ADD] initial proj

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Sopkathon-iOS-Extension",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Sopkathon-iOS-Extension",
+            targets: ["Sopkathon-iOS-Extension"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Sopkathon-iOS-Extension"),
+        .testTarget(
+            name: "Sopkathon-iOS-ExtensionTests",
+            dependencies: ["Sopkathon-iOS-Extension"]),
+    ]
+)
---
+++ Tests/Sopkathon-iOS-ExtensionTests/Sopkathon_iOS_ExtensionTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Sopkathon_iOS_Extension
+
+final class Sopkathon_iOS_ExtensionTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
